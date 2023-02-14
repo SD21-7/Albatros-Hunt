07d74805a81623617164eb5fb0adfe6daab81dd1
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2023-02-14 08:53:59 +0100

    push 1

---
+++ Assets/Master/Scripts/TargetController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    private void Update()
+    {
+        OnMouseOver();
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
