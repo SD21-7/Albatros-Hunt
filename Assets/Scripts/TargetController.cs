cec2660d64c0fbcb7a79dd388344a0dd7336874d
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2023-02-14 10:50:28 +0100

    TargetController.cs basic beweging en death script

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -5,15 +5,44 @@ using UnityEngine;
 
 public class TargetController : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+    public bool MovingRight = true;
+    private float _verticalmovement;
+
+    [Range(0, 10)] public float _jumpforce;
+    [Range(-10, 10)] public float speed;
+    [Range(0, 5)] public float _jumprate;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        InvokeRepeating("Fly", 0, _jumprate);
+    }
+
     private void Update()
     {
+        _verticalmovement = _rigidbody2D.velocity.y;
         OnMouseOver();
-        
+        _rigidbody2D.velocity = new Vector2(speed, _verticalmovement);
     }
 
     private void OnMouseOver()
     {
         if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Fly()
+    {
+        _rigidbody2D.velocity = new Vector2(0, _jumpforce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        speed = -speed;
+        if (col.gameObject.CompareTag("TopCol"))
         {
             Destroy(gameObject);
         }
