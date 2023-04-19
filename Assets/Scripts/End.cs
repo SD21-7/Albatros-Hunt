d4bb0727757bca8f594efd8e4febb7fb316dfa4d
Ruben190404 <92361480+Ruben190404@users.noreply.github.com>
2023-04-19 09:07:52 +0200

    scripts clean up and end to main menu

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    public float timer = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene("Main_Menu");
+        }
+    }
+}
